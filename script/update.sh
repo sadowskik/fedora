bc57c6cacb645547e9b169fb0395a70e87d44119
Krzysztof Sadowski <k.sadowskik@gmail.com>
2017-02-15 21:33:29 +0100

    upgrade

--- script/update.sh
+++ script/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 if [[ $UPDATE  =~ true || $UPDATE =~ 1 || $UPDATE =~ yes ]]; then
     echo "==> Applying updates"
-    dnf -y update
+    dnf -y upgrade
 
     # reboot
     echo "Rebooting the machine..."
